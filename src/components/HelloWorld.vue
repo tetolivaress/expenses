b6bd3231277f3227b3779ed782aba09bbae1f3a8
tetolivares <tetolivares@gmail.com>
2021-01-11 18:20:51 -0400

    added form to modal

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -38,12 +38,56 @@
 
         <v-divider></v-divider>
 
+        <v-form v-model="valid">
+          <v-container>
+            <v-row>
+              <v-col
+                cols="12"
+                md="4"
+              >
+                <v-text-field
+                  v-model="firstname"
+                  :rules="nameRules"
+                  :counter="10"
+                  label="First name"
+                  required
+                ></v-text-field>
+              </v-col>
+
+              <v-col
+                cols="12"
+                md="4"
+              >
+                <v-text-field
+                  v-model="lastname"
+                  :rules="nameRules"
+                  :counter="10"
+                  label="Last name"
+                  required
+                ></v-text-field>
+              </v-col>
+
+              <v-col
+                cols="12"
+                md="4"
+              >
+                <v-text-field
+                  v-model="email"
+                  :rules="emailRules"
+                  label="E-mail"
+                  required
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-form>
+
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn
             color="primary"
             text
-            @click="dialog = false"
+            @click="saveExpense"
           >
             Add
           </v-btn>
@@ -59,6 +103,11 @@ export default {
 
   data: () => ({
     newExpense: false
-  })
+  }),
+  methods: {
+    saveExpense () {
+
+    }
+  }
 }
 </script>
