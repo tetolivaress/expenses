6f93fcc9022dca3cc02677f483cc55fff08fbb06
tetolivares <tetolivares@gmail.com>
2021-01-11 09:25:00 -0400

    remoded firebase data

--- src/store/db.js
+++ src/store/db.js
@@ -1,16 +1,8 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
+import firebaseConfig from '../../firebaseConfig.json'
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyCFIntNoACk3cad1pQ_RoBPlFgYzrYRTRY',
-  authDomain: 'diary-book-94707.firebaseapp.com',
-  databaseURL: 'https://diary-book-94707.firebaseio.com',
-  projectId: 'diary-book-94707',
-  storageBucket: 'diary-book-94707.appspot.com',
-  messagingSenderId: '475895040261',
-  appId: '1:475895040261:web:48bc5c9e7555f467cc8f6c',
-  measurementId: 'G-9V730E1L2Q'
-}
+
 // firebase
 //   .initializeApp(firebaseConfig)
 //   .firestore()
