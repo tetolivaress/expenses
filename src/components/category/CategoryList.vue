d7d9e7283a612c3e884ec29eda7324a18f8210c3
tetolivaress <tetolivares@gmail.com>
2021-02-07 17:11:39 -0400

    if a category is used is not posible delete it

--- src/components/category/CategoryList.vue
+++ src/components/category/CategoryList.vue
@@ -20,7 +20,7 @@
         <v-list-item-title v-text="category.name"></v-list-item-title>
       </v-list-item-content>
 
-      <v-list-item-action>
+      <v-list-item-action v-if="!expenses.filter(expense => expense.categoryId == category.id).length">
         <v-btn icon @click="removeCategory(category)">
           <v-icon color="red lighten-1">mdi-close</v-icon>
         </v-btn>
@@ -33,6 +33,7 @@ import { mapState, mapActions } from 'vuex'
 export default {
   computed: {
     ...mapState({
+      expenses: ({ expense }) => expense.expenses,
       user: ({ user }) => user.user,
       categories: ({ category }) => category.categories
     })
