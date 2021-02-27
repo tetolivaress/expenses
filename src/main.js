3d911c86b013b5f18cb863e253193cb153326b91
Julio Perdomo <jperdomocorrea@gmail.com>
2021-02-27 16:14:01 -0400

    Set moment default lang to es

--- src/main.js
+++ src/main.js
@@ -5,15 +5,15 @@ import store from './store'
 import './registerServiceWorker'
 import vuetify from './plugins/vuetify'
 import { i18n } from './plugins/i18n'
-import moment from 'moment'
+import vueMoment from './plugins/moment'
 
 Vue.config.productionTip = false
-moment.locale('es')
 
 new Vue({
   router,
   store,
   vuetify,
   i18n,
+  vueMoment,
   render: h => h(App)
 }).$mount('#app')
